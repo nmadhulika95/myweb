2a70fa76c5e8f566116fd04ec458c6a42e597346
nmadhulika95 <nmadhulika.95@gmail.com>
2022-07-07 13:18:42 -0400

    z3

--- main.tf
+++ main.tf
@@ -3,6 +3,7 @@ provider "aws" {
   region = var.region
   profile = var.profile
 }
+poipoipoi
 resource "aws_instance" "web7" {
   ami           = "${lookup(var.ami, var.region)}"
   instance_type = "${lookup(var.instancetype, var.region)}"
